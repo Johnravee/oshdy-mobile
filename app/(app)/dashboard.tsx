3f241f2252052ff8b718aea821d934dd8bc226e2
John Rave Mimay <johnravemimayn@gmail.com>
2025-05-12 15:50:47 +0800

    refactor: simplify state initialization and improve modal structure in Dashboard component

--- app/(app)/dashboard.tsx
+++ app/(app)/dashboard.tsx
@@ -9,23 +9,21 @@ import { IMAGES } from '@/constants/Images';
 import CustomModal from '@/components/ui/custom-modal';
 import LottieView from 'lottie-react-native';
 
-
 export default function Dashboard() {
   const router = useRouter();
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const [showCalendar, setShowCalendar] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [searchingModal, setSearchingModal] = useState(false);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [searchResult, setSearchResult] = useState(false); //Modal for search result
+  const [schedData, setSchedData] = useState(true);
   const [activeTab, setActiveTab] = useState<string>('home');
-  const [searchingModal, setSearchingModal ] = useState<boolean>(false)
-  const [selectedDate, setSelectedDate] = useState<string>('');
-  const [searchResult, setSearchResult] = useState<boolean>(false); {/* temporary lang for demo */}
-  const [schedData, setSchedData] = useState<boolean>(false); {/* temporary lang for demo */}
-  
+
 
   const handleTabPress = (tabName: string) => {
     setActiveTab(tabName);
   };
 
-  // Manage showing the calendar after modal opens
   useEffect(() => {
     if (modalVisible) {
       const timeout = setTimeout(() => {
@@ -38,59 +36,24 @@ export default function Dashboard() {
   }, [modalVisible]);
 
   const cards = [
-    {
-      id: 1,
-      title: 'Schedule Your Event',
-      icon: "calendar-plus-o",
-      background: IMAGES.yellowcardbg,
-    },
-    {
-      id: 2,
-      title: 'Check Event Status',
-      icon: "clock-o",
-      background: IMAGES.tealroundedcardbg,
-    },
-    {
-      id: 3,
-      title: 'Review Past Bookings',
-      icon: "history",
-      background: IMAGES.orangecardbg,
-    },
-    {
-      id: 4,
-      title: 'Explore Event Designs',
-      icon: "magic",
-      background: IMAGES.lighttealboxcardbg,
-    },
-    {
-      id: 5,
-      title: 'Explore Event Packages',
-      icon: "compass",
-      background: IMAGES.navycardbg,
-    },
-    {
-      id: 6,
-      title: 'View Menu Options',
-      icon: "delicious",
-      background: IMAGES.yellowredcardbg,
-    }
-    ]
+    { id: 1, title: 'Schedule Your Event', icon: "calendar-plus-o", background: IMAGES.yellowcardbg },
+    { id: 2, title: 'Check Event Status', icon: "clock-o", background: IMAGES.tealroundedcardbg },
+    { id: 3, title: 'Review Past Bookings', icon: "history", background: IMAGES.orangecardbg },
+    { id: 4, title: 'Explore Event Designs', icon: "magic", background: IMAGES.lighttealboxcardbg },
+    { id: 5, title: 'Explore Event Packages', icon: "compass", background: IMAGES.navycardbg },
+    { id: 6, title: 'View Menu Options', icon: "delicious", background: IMAGES.yellowredcardbg }
+  ];
 
   return (
     <SafeAreaView className="flex-1 bg-primary">
       <StatusBar hidden={true} />
-
+      
       {/* Header */}
       <View className="flex justify-between items-start mt-5 gap-5 px-5">
         <Text className="text-white text-4xl font-bold">
           Welcome back! Let’s Plan Your Event.
         </Text>
-
-        <TouchableHighlight
-          onPress={() => setModalVisible(true)}
-          underlayColor="#ddd"
-          className="rounded-full"
-          >
+        <TouchableHighlight onPress={() => setModalVisible(true)} underlayColor="#ddd" className="rounded-full">
           <View className="h-16 w-full bg-white rounded-lg flex-row items-center justify-between px-5">
             <Text className="text-[#33333] text-lg font-semibold">Check Available Schedules</Text>
             <FontAwesome name="calendar" size={20} color="#33333" />
@@ -99,25 +62,13 @@ export default function Dashboard() {
       </View>
 
       {/* Modal for Calendar */}
-      <Modal
-        animationType="fade"
-        transparent
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
+      <Modal animationType="fade" transparent visible={modalVisible} onRequestClose={() => setModalVisible(false)}>
         <View className="flex-1 bg-transparent justify-end">
-          {/* Background overlay to close modal on press */}
           <Pressable className="flex-1 w-full" onPress={() => setModalVisible(false)} />
-          
           <View className="w-full h-1/2 bg-white rounded-t-3xl p-5 relative shadow-lg">
-            {/* Close button */}
-            <Pressable
-              onPress={() => setModalVisible(false)}
-              className="absolute top-4 right-4 z-10"
-            >
+            <Pressable onPress={() => setModalVisible(false)} className="absolute top-4 right-4 z-10">
               <FontAwesome name="close" size={24} color="#333" />
             </Pressable>
-
             <Text className="text-lg font-bold mb-4 text-center">Select a Date</Text>
 
             {/* Loading Spinner */}
@@ -132,18 +83,15 @@ export default function Dashboard() {
             {showCalendar && (
               <Calendar
                 singleSelectMode
-                onChange={(date) => 
-                {
-                  setSelectedDate(date)
-                  setModalVisible(false)
-                  setShowCalendar(false)
-                  setSearchingModal(true)
-                }
-                }
+                onChange={(date) => {
+                  setSelectedDate(date);
+                  setModalVisible(false);
+                  setShowCalendar(false);
+                  setSearchingModal(true);
+                }}
                 pastYearRange={0}
                 startDate={new Date().toISOString().split('T')[0]}
                 disabledBeforeToday={true}
-                
               />
             )}
           </View>
@@ -151,136 +99,85 @@ export default function Dashboard() {
       </Modal>
 
       {/* Searching Modal */}
-      <CustomModal
-        visible={searchingModal}
-        onClose={() => true}
-      >
-
+      <CustomModal visible={searchingModal} onClose={() => true}>
         <View className="flex-1 justify-center items-center bg-black/50">
-                      <View className="bg-white rounded-2xl p-5 w-11/12 h-auto justify-center items-center">
-                        {/* Close icon */}
-                        <Pressable
-                          onPress={() => setSearchingModal(false)}
-                          className="absolute top-2 right-2 p-2 rounded-full"
-                        >
-                          <FontAwesome name="close" size={20} color="#333" />
-                        </Pressable>
-              
-                        {/* Lottie Animation */}
-                        <LottieView
-                          source={require('../../assets/images/lottie/searching.json')}
-                          autoPlay
-                          loop
-                          style={{ width: 150, height: 150 }}
-                        />
-              
-                        {/* Title */}
-                        <Text className="text-2xl font-bold text-center mt-2 mb-4 text-dark">
-                          Searching for Available Schedules
-                        </Text>
-              
-                        {/* Message */}
-                        <Text className="text-base text-center text-gray-600 mb-5">
-                        Please hold while we check our catering calendar for availability on {selectedDate}. This may take a few moments as we verify scheduling and ensure our team can accommodate your event requirements.
-                        </Text>
-              
-                        {/* Divider */}
-                        <View className="my-4 border-t border-gray-300 w-full" />
-              
-                        {/* Note */}
-                        <Text className="text-base text-center text-gray-600">
-                        You will be notified as soon as the scheduling results are ready. Please do not refresh or close this window during the process.
-                        </Text>
-                      </View>
-                    </View>
-
+          <View className="bg-white rounded-2xl p-5 w-11/12 h-auto justify-center items-center">
+            <Pressable onPress={() => setSearchingModal(false)} className="absolute top-2 right-2 p-2 rounded-full">
+              <FontAwesome name="close" size={20} color="#333" />
+            </Pressable>
+            <LottieView
+              source={require('../../assets/images/lottie/searching.json')}
+              autoPlay
+              loop
+              style={{ width: 150, height: 150 }}
+            />
+            <Text className="text-2xl font-bold text-center mt-2 mb-4 text-dark">
+              Searching for Available Schedules
+            </Text>
+            <Text className="text-base text-center text-gray-600 mb-5">
+              Please hold while we check our catering calendar for availability on {selectedDate}. This may take a few moments.
+            </Text>
+            <View className="my-4 border-t border-gray-300 w-full" />
+            <Text className="text-base text-center text-gray-600">
+              You will be notified as soon as the scheduling results are ready. Please do not refresh or close this window.
+            </Text>
+          </View>
+        </View>
       </CustomModal>
 
       {/* Available Schedule Modal */}
-      <CustomModal
-        visible={searchResult}
-        onClose={() => true}
-      >
-
+      <CustomModal visible={searchResult} onClose={() => true}>
         <View className="flex-1 justify-center items-center bg-black/50">
-                      <View className="bg-white rounded-2xl p-5 w-11/12 h-auto justify-center items-center">
-                        {/* Close icon */}
-                        <Pressable
-                          onPress={() => setSearchingModal(false)}
-                          className="absolute top-2 right-2 p-2 rounded-full"
-                        >
-                          <FontAwesome name="close" size={20} color="#333" />
-                        </Pressable>
-              
-                        {/* Lottie Animation */}
-                        <LottieView
-                          source={schedData ? require('../../assets/images/lottie/check.json') : require('../../assets/images/lottie/notfound.json')}
-                          autoPlay
-                          loop
-                          style={{ width: 150, height: 150 }}
-                        />
-              
-                        {/* Title */}
-                        <Text className="text-2xl font-bold text-center mt-2 mb-4 text-dark">
-                          {schedData ? 'Confirmed Availability': 'Not Available'}
-                        </Text>
-              
-                        {/* Message */}
-                        <Text className="text-base text-center text-gray-600 mb-5">
-                        Your selected date is {selectedDate} {schedData ? 'available' : 'not available'}. Please proceed with your reservation or choose a different date if necessary.
-                        </Text>
-              
-                        {/* Divider */}
-                        <View className="my-4 border-t border-gray-300 w-full" />
-              
-                        {/* Note */}
-                        <Text className="text-base text-center text-gray-600">
-                        If you need to make any changes or select a different date, please do so now.
-                        </Text>
-                      </View>
-                    </View>
-
+          <View className="bg-white rounded-2xl p-5 w-11/12 h-auto justify-center items-center">
+            <Pressable onPress={() => setSearchingModal(false)} className="absolute top-2 right-2 p-2 rounded-full">
+              <FontAwesome name="close" size={20} color="#333" />
+            </Pressable>
+            <LottieView
+              source={schedData ? require('../../assets/images/lottie/check.json') : require('../../assets/images/lottie/notfound.json')}
+              autoPlay
+              loop
+              style={{ width: 150, height: 150 }}
+            />
+            <Text className="text-2xl font-bold text-center mt-2 mb-4 text-dark">
+              {schedData ? 'Confirmed Availability' : 'Not Available'}
+            </Text>
+            <Text className="text-base text-center text-gray-600 mb-5">
+              Your selected date is {selectedDate} {schedData ? 'available' : 'not available'}. Please proceed with your reservation or choose a different date if necessary.
+            </Text>
+            <View className="my-4 border-t border-gray-300 w-full" />
+            <Text className="text-base text-center text-gray-600">
+              If you need to make any changes or select a different date, please do so now.
+            </Text>
+          </View>
+        </View>
       </CustomModal>
 
       {/* Main Content */}
-      <View className="h-full w-full rounded-t-2xl bg-white mt-10 ">
-        
-         <View className="w-full h-auto flex flex-row flex-wrap justify-center  bg-transparent items-center mt-10 py-10 px-4 ">
-               {cards.map((card) => (
-                 <Pressable  className="w-1/2 sm:w-full rounded-3xl overflow-hidden p-3" key={card.id}>
-                           <ImageBackground
-                             source={card.background}
-                             className="w-full aspect-[4/3] overflow-hidden px-5 py-4 justify-start"
-                             imageStyle={{ borderRadius: 24 }}
-                           >
-                             <Text className="text-white font-extrabold mt-2">
-                               <FontAwesome name={card.icon as keyof typeof FontAwesome.glyphMap} size={30} color="#ffffff" />
-                             </Text>
-                             <Text className="text-white text-xl font-bold mt-4">
-                               {card.title}
-                             </Text>
-                           </ImageBackground>
-                 </Pressable>
-               ))}
-               </View>
-         {/* Floating Tab Bar */}
-        <View className='w-full h-auto flex justify-center items-center py-safe-or-10 px-5'>
-          <View className='border rounded-full py-1 w-auto bg-transparent shadow-lg'>
-            <View className="absolute right-[-10%]">
-                <View className="h-10 w-10 bg-primary rounded-full opacity-40 top-[-80%] right-[90%]" />
-                <View className="h-14 w-14 bg-primary rounded-full opacity-50 top-[-70%] right-[5%]" />
-            </View>           
+      <View className="h-full w-full rounded-t-2xl bg-white mt-10">
+        <View className="w-full h-auto flex flex-row flex-wrap justify-center bg-transparent items-center mt-10 py-10 px-4">
+          {cards.map((card) => (
+            <Pressable className="w-1/2 sm:w-full rounded-3xl overflow-hidden p-3" key={card.id}>
+              <ImageBackground
+                source={card.background}
+                className="w-full aspect-[4/3] overflow-hidden px-5 py-4 justify-start"
+                imageStyle={{ borderRadius: 24 }}
+              >
+                <Text className="text-white font-extrabold mt-2">
+                  <FontAwesome name={card.icon as keyof typeof FontAwesome.glyphMap} size={30} color="#ffffff" />
+                </Text>
+                <Text className="text-white text-xl font-bold mt-4">{card.title}</Text>
+              </ImageBackground>
+            </Pressable>
+          ))}
+        </View>
 
+        {/* Floating Tab Bar */}
+        <View className="w-full h-auto flex justify-center items-center py-safe-or-10 px-5">
+          <View className="border rounded-full py-1 w-auto bg-transparent shadow-lg">
             <FloatingTabBar onTabPress={handleTabPress} />
-            
-            <View className="absolute bottom-0 left-0 w-full h-[25%] items-start justify-end z-[-1]">
-              <View className="absolute w-[30%] aspect-square bg-primary rounded-full opacity-40 top-[-10%] left-[-10%]" />
-            </View>
           </View>
         </View>
       </View>
-
-     
     </SafeAreaView>
   );
 }
